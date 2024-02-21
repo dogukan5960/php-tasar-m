23b1fc2523ae04b9a8983881f30b5d0f435297e8
dogukan5960 <146706726+dogukan5960@users.noreply.github.com>
2024-02-21 14:22:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>evet</title>
+	<style type="text/css">
+		.arkaplan{
+ margin:auto;
+ margin-top:20px;
+ height:auto;
+ width:500px;
+ color:gray;
+ border:10px solid brown;
+ padding:25px;
+ background:white;
+ }
+	</style>
+</head>
+<body>
+	<div class="arkaplan">
+<table>
+	<tr>
+<td><h1>Kodlama Eğitimi</h1></td>
+</tr>
+<tr>
+	<td><h6>İletişim Formu</h6></td>
+</tr>
+<tr>
+	<td>-Kodlama eğitimlerinde sizde yanımızda olmak ister
+
+		misin? Cevabınız EVET ise lütfen iletişim formunu doldurunuz.<p></p></td>
+</tr>
+
+<tr>
+	<td ><input type="text" name="Ad" value="Ad"></td>
+	<td ><input type="text" name="soyad" value="Soyad"></td>
+</tr>
+<tr>
+	<td><input type="text" name="telefon" value="(xxx)xxx xx xx"></big></td>
+	<td><input type="radio" name="Erkek">Kadın<input type="radio" name="Kadın">Erkek</td>
+
+</tr>
+<tr>
+	<td><input type="email" name="" value="E-Mail"></td>
+</tr>
+<tr><td><textarea>Öz Geçmiş</textarea></td></tr>
+<tr>
+	<td><input type="submit" name="" value="Formu gönder" style="background-color: green;"></td>
+</tr>
+
+</table>
+</div>
+</body>
+</html>
